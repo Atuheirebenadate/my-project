71fceba599bcf8b1dc9230e6f3e0662f07c4ca41
Atuheirebenadate <benadateatuheire@gmail.com>
2022-02-12 14:50:56 +0300

    first commit

---
+++ src/main/kotlin/student.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val name="ATUHEIRE BENADATE"
+    println(name)
+    var age:Byte=21
+    println(age)
+    val myNumb:Long=776586893
+    println(myNumb)
+    var kenyan:Boolean=true
+    var isnotkenyan:Boolean=false
+    println (isnotkenyan)
+
+var citizen:Boolean=true
+    var isnotacitizen=false
+    println(isnotacitizen)
+    val myText:String="90kgs"
+    println (myText)
+}
